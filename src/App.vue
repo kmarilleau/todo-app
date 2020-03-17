84d414df4654f29906fecff8e33f1433bce229b6
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-03-18 00:20:37 +0100

    Add Todo Container and Item

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="todo-wrapper">
+      <div class="todo-container">
+        <div class="todo-list">
+          <div class="todo-item">
+            <div class="todo-item-content">
+              <div class="todo-item-content-title">
+                Test
+              </div>
+              <div class="todo-item-content-description">
+                Content of todo
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+};
 </script>
 
 <style>
@@ -22,7 +31,36 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: cornflowerblue;
   margin-top: 60px;
+}
+
+.todo-wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.todo-container {
+  width: 400px;
+  min-height: 400px;
+  background-color: cornflowerblue;
+  border-radius: 5px;
+}
+
+.todo-item {
+  background-color: beige;
+  min-height: 50px;
+  margin: 10px;
+  padding: 10px;
+  color: cornflowerblue;
+  border-radius: 5px;
+  font-size: 23px;
+}
+.todo-item-content-title {
+  font-weight: bold;
+}
+.todo-item-content-description {
+  font-size: 19px;
 }
 </style>
